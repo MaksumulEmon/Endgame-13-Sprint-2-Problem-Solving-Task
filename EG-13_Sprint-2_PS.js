608fc4d491c01c7c75326ed501e9833e813838c1
MaksumulEmon <maksumulemon300@gmail.com>
2026-08-28 15:48:23 +0600

    Initial commit - Find Maximum

--- EG-13_Sprint-2_PS.js
+++ EG-13_Sprint-2_PS.js
@@ -1,7 +1,17 @@
 // 1. Reverse a String
-
 function reverseString(str) {
     return str.split("").reverse().join("");
 }
 
 console.log(reverseString("hello"));
+
+
+// 2. Find Maximum
+function findMax(nums) {
+    if (nums.length === 0) return undefined; // Handle empty array edge case
+    return Math.max(...nums);
+}
+
+console.log(findMax([3, 7, 2, 9, 5]));
+
+
