e7c6aa052a05129b1ed27d1606b4c3b1fcd455c1
MaksumulEmon <maksumulemon300@gmail.com>
2026-08-29 20:58:53 +0600

    Initial commit -Longest Substring Without Repeating Characters

--- EG-13_Sprint-2_PS.js
+++ EG-13_Sprint-2_PS.js
@@ -71,3 +71,28 @@ function groupAnagrams(strs) {
 }
 
 console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]));
+
+
+
+// 9. Longest Substring Without Repeating Characters
+function lengthOfLongestSubstring(s) {
+    let set = new Set();
+    let left = 0;
+    let max = 0;
+
+    for (let right = 0; right < s.length; right++) {
+
+        while (set.has(s[right])) {
+            set.delete(s[left]);
+            left++;
+        }
+
+        set.add(s[right]);
+        max = Math.max(max, right - left + 1);
+    }
+
+    return max;
+}
+
+console.log(lengthOfLongestSubstring("abcabcbb"));
+// Output: 3
